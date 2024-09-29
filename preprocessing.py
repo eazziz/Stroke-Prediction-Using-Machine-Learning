e1bbf26b5459aa32ca0a80a6057b38666681dd0d
eazziz <azzizedredelijah@gmail.com>
2024-09-28 20:57:02 -0400

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,21 +1,30 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from imblearn.over_sampling import SMOTE
 
-def preprocess_data():
-    df = pd.read_csv('./healthcare-dataset-stroke-data.csv')
-    # Data cleaning steps here (drop duplicates, handle missing values)
-    # Encoding and normalization steps here
+def preprocess_data(df):
+    #Drop missing values and duplicates
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
+    df.drop('id', axis=1, inplace=True)
 
+    #Encoding categorical variables
+    le = LabelEncoder()
+    df['gender'] = le.fit_transform(df['gender'])
+    df['ever_married'] = le.fit_transform(df['ever_married'])
+    df['work_type'] = le.fit_transform(df['work_type'])
+    df['Residence_type'] = le.fit_transform(df['Residence_type'])
+    df['smoking_status'] = le.fit_transform(df['smoking_status'])
+
+    #Normalization
+    scaler = StandardScaler()
+    numerical_features = ['age', 'hypertension', 'heart_disease', 'avg_glucose_level', 'bmi']
+    df[numerical_features] = scaler.fit_transform(df[numerical_features])
+
+    #Address imbalance with SMOTE
     pre_X = df.drop('stroke', axis=1)
     pre_Y = df['stroke']
-
-    # Addressing imbalance with SMOTE
     smote = SMOTE(random_state=42)
     X, Y = smote.fit_resample(pre_X, pre_Y)
 
-    # Train-test split
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
-
-    return X_train, X_test, Y_train, Y_test, scaler  # Return the scaler if you need it later
+    return X, Y, scaler, df
